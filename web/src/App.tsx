79e72223a61fd9f8053b22ac5718ccd78461ef9a
jeanalcides <jean.ap44@gmail.com>
2022-05-02 21:52:51 -0300

    Configurando Tailwind, Criando Widget, conceitos de acessibilidade e Utilizando PopOver

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
+import { Widget } from "./components/Widget";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <h1>Hello World!</h1>
+export function App(){
+  return(
+    <Widget />
   )
 }
-
-export default App
